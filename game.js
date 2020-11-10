d4a8a14d1c8e797176417d3d9caab95edb3f053f
spark-o-petit <alexchoi970@gmail.com>
2020-11-10 19:40:02 +0900

    Completed function building div tags

--- game.js
+++ game.js
@@ -1,16 +1,14 @@
-const container = document.getElementsByClassName('container')
+const container = document.querySelector('div#container')
+
+
 
 function buildGrid(numberRows,numberCols) {
     let totalNumBoxes = numberRows * numberCols;
     for (let i = 0; i < totalNumBoxes; i++) {
-        let builidngBox = document.createElement("div");
-        builidngBox.addEventListener("mouseover", () => {
-
-        })
-
-
-        container.appendChild(builidngBox);
-
+        const buildingBox = document.createElement('div');
+        
+        container.appendChild(buildingBox);
+        i = i ++;
         
     }
 }
@@ -20,6 +18,6 @@ function buildGrid(numberRows,numberCols) {
 
 //main code
 
-let gridRows = prompt("How many rows in your head? (WARN : It should be no more than 100)");
-let gridCols = prompt("Then, how many columns in your head? (WARN : It should be no more than 100)");
+// let gridRows = prompt("How many rows in your head? (WARN : It should be no more than 100)");
+// let gridCols = prompt("Then, how many columns in your head? (WARN : It should be no more than 100)");
 
