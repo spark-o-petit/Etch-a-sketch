164d851186fe4b4c4005bac942312fc313bee0c3
Jeong-hwan Choi <jeonghwan@Jeong-hwans-iMac.local>
2020-11-24 14:29:34 +0900

    Added event listener

--- game.js
+++ game.js
@@ -1,13 +1,20 @@
 const container = document.getElementById('container')
 
+const ponderColor = ["(60, 84, 134)", "(113, 139, 190)", "(182, 198, 211)", "(127, 133, 137)", "(158, 144, 130)", "(235, 186, 155)", "(241, 221, 183)"]
 
 
 function buildGrid(numberRows,numberCols) {
     let totalNumBoxes = numberRows * numberCols;
+    function randomPonderColor () {
+
+    }
     for (let i = 0; i < totalNumBoxes; i++) {
         const buildingBox = document.createElement('div');
         buildingBox.style.height = `${1000/numberCols}px`;
         buildingBox.style.width = `${1000/numberCols}px`;
+        buildingBox.addEventListener ('mouseover', () => {
+            buildingBox.style.backgroundColor = `rgb${ponderColor[Math.floor(Math.random() * 7)]}`;
+        });
         container.appendChild(buildingBox);
         i = i ++;
         
