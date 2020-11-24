2e6c325a0606c913c4f3daa305d13df9e5678420
Jeong-hwan Choi <jeonghwan@Jeong-hwans-iMac.local>
2020-11-24 14:11:51 +0900

    Compledted building grid in constant width

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('div#container')
+const container = document.getElementById('container')
 
 
 
@@ -6,7 +6,8 @@ function buildGrid(numberRows,numberCols) {
     let totalNumBoxes = numberRows * numberCols;
     for (let i = 0; i < totalNumBoxes; i++) {
         const buildingBox = document.createElement('div');
-        
+        buildingBox.style.height = `${1000/numberCols}px`;
+        buildingBox.style.width = `${1000/numberCols}px`;
         container.appendChild(buildingBox);
         i = i ++;
         
@@ -17,6 +18,9 @@ function buildGrid(numberRows,numberCols) {
 
 
 //main code
+
+buildGrid (16, 16);
+
 
 // let gridRows = prompt("How many rows in your head? (WARN : It should be no more than 100)");
 // let gridCols = prompt("Then, how many columns in your head? (WARN : It should be no more than 100)");
